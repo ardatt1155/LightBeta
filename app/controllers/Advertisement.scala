e085318e01eba1867664873dafac996b54e378ea
Arijit Datta <arijit.datta@westwing.de>
2015-12-10 23:06:31 +0100

    Check 006

--- app/controllers/Advertisement.scala
+++ app/controllers/Advertisement.scala
@@ -3,6 +3,8 @@ package controllers
 import play.api._
 import play.api.mvc._
 import models._
+import java.util._
+import java.text._
 
 class Advertisement extends Controller {
 
@@ -10,6 +12,39 @@ class Advertisement extends Controller {
   	val x = new Advertisement()
   	System.out.println(x.toString())
     Ok("Hello world")
+  }
+
+  def get(uid: Int) = Action { request =>
+  	val sort = request.queryString.get("sort").flatMap(_.headOption).getOrElse("uid");
+  	Ok("Got " + uid);
+  }
+
+  def post(uid: Int) = Action { request =>
+	request.body.asJson.map { json =>
+		val df = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+		val title = (json \ "title").asOpt[String].getOrElse(null)
+		val price = (json \ "price").asOpt[Int].getOrElse(null)
+		val mileage = (json \ "mileage").asOpt[Int].getOrElse(null)
+		val registration = (json \ "registration").asOpt[String].getOrElse(null)
+		val state = (json \ "state").asOpt[Int].getOrElse(null)
+		try {
+			val date = df.parse(registration)
+			if (title == null || price == null || mileage == null || state == null) {
+				BadRequest("Missing fields")
+			} else {
+				System.out.println(date)
+				Ok(title + price + mileage)
+			}
+		} catch {
+			case e: Exception => BadRequest("Bad fields")
+		}
+	}.getOrElse {
+      BadRequest("Json Missing")
+    }
+  }
+
+  def delete(uid: Int) = Action {
+  	Ok("Got " + uid);
   }
 
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,26 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table advertisement (
+  uid                       bigint auto_increment not null,
+  title                     varchar(255),
+  price                     integer,
+  state                     tinyint(1) default 0,
+  mileage                   integer,
+  registration              datetime(6),
+  constraint pk_advertisement primary key (uid))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table advertisement;
+
+SET FOREIGN_KEY_CHECKS=1;
+
