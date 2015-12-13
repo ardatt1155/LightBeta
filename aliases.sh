d8eea5d9c1b866b523db4af59207624a6242d0b9
Arijit Datta <arijit.datta@westwing.de>
2015-12-13 15:35:45 +0100

    AngularTodo 019

---
+++ aliases.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+alias nodemon="./node_modules/nodemon/bin/nodemon.js"
+alias node-debug="./node_modules/node-inspector/bin/node-debug.js"
+alias webpack="./node_modules/webpack/bin/webpack.js"
+alias node-uuid="./node_modules/node-uuid/bin/uuid"
+alias karma="./node_modules/karma/bin/karma"
+alias protractor="./node_modules/protractor/bin/protractor"
