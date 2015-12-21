92254fb70773e7d05843f745efeefd234fefd7cf
Arijit Datta <arijit.datta@westwing.de>
2015-12-21 20:17:06 +0100

    Check 002

---
+++ aliases.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+alias codecept="./vendor/bin/codecept"
+alias markdown="./vendor/bin/markdown"
+alias phpunit="./vendor/bin/phpunit"
+alias yii="./yii"
---
+++ migrations/m151221_191301_Migration001.php
@@ -0,0 +1,15 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151221_191301_Migration001 extends Migration
+{
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+}
